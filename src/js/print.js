5333526a3f3eeed872fb93e7a730da839d4315a8
wfsovereign <never_fall@sohu.com>
2015-07-08 08:56:45 +0800

    增加命名空间，将html生成放入其中

--- src/js/print.js
+++ src/js/print.js
@@ -18,6 +18,38 @@ $(function () {
 });
 
 
+
+var NS = NS ||{};
+
+NS.getGoalMatrixHtml = (function (){
+    return function (goalMatrix){
+        var goalMatrixHtml ="";
+        _.each(goalMatrix, function (item) {
+            goalMatrixHtml += "<tr>";
+            for (var i = 0; i < item.length; i++) {
+                goalMatrixHtml += "<td>" + item[i] + "</td>"
+            }
+            goalMatrixHtml += "</tr>";
+        });
+        return  goalMatrixHtml
+    }
+}());
+
+NS.getGoalArraySummaryHtml = (function () {
+    return function (goalArraySummary) {
+        var i = 0,goalArraySummaryHtml = "<tr>";
+        _.each(goalArraySummary, function (item) {
+            goalArraySummaryHtml += "<td>" + item + "</td>";
+            if ((i + 1) % 5 == 0) {
+                goalArraySummaryHtml += "<tr></tr>"
+            }
+            i++;
+        });
+        return goalArraySummaryHtml
+    }
+}());
+
+
 function printToTable() {
     var matrix = new Matrix(5);
 
@@ -33,74 +65,86 @@ function printToTable() {
         horizontalLineSummary = matrix.getHorizontalLineSummary(), backSlashSummary = matrix._calculateBackSlashSummary(),
         slashSummary = matrix._calculateSlashSummary();
 
-    _.each(matrixResult, function (item) {
-        matrixHtml1 += "<tr>";
-        for (var i = 0; i < item.length; i++) {
-            matrixHtml1 += "<td>" + item[i] + "</td>"
-        }
-        matrixHtml1 += "</tr>";
-    });
+    //_.each(matrixResult, function (item) {
+    //    matrixHtml1 += "<tr>";
+    //    for (var i = 0; i < item.length; i++) {
+    //        matrixHtml1 += "<td>" + item[i] + "</td>"
+    //    }
+    //    matrixHtml1 += "</tr>";
+    //});
+    //
+    //_.each(matrixTranspose, function (item) {
+    //    matrixHtml2 += "<tr>";
+    //    for (var i = 0; i < item.length; i++) {
+    //        matrixHtml2 += "<td>" + item[i] + "</td>"
+    //    }
+    //    matrixHtml2 += "</tr>";
+    //});
 
-    _.each(matrixTranspose, function (item) {
-        matrixHtml2 += "<tr>";
-        for (var i = 0; i < item.length; i++) {
-            matrixHtml2 += "<td>" + item[i] + "</td>"
-        }
-        matrixHtml2 += "</tr>";
-    });
 
-    var i = 0;
-    verticalLineSummaryHtml += "<tr>";
+    matrixHtml1 = NS.getGoalMatrixHtml(matrixResult);
+    matrixHtml2 = NS.getGoalMatrixHtml(matrixTranspose);
 
-    _.each(verticalLineSummary, function (item) {
 
-        verticalLineSummaryHtml += "<td>" + item + "</td>";
-        if ((i + 1) % 5 == 0) {
-            verticalLineSummaryHtml += "<tr></tr>"
-        }
-        i++;
-    });
+    verticalLineSummaryHtml = NS.getGoalArraySummaryHtml(verticalLineSummary);
+    horizontalLineSummaryHtml = NS.getGoalArraySummaryHtml(horizontalLineSummary);
 
-    var j = 0;
-    horizontalLineSummaryHtml = "<tr>";
+    //var i = 0;
+    //verticalLineSummaryHtml += "<tr>";
+    //
+    //_.each(verticalLineSummary, function (item) {
+    //
+    //    verticalLineSummaryHtml += "<td>" + item + "</td>";
+    //    if ((i + 1) % 5 == 0) {
+    //        verticalLineSummaryHtml += "<tr></tr>"
+    //    }
+    //    i++;
+    //});
+    //
+    //var j = 0;
+    //horizontalLineSummaryHtml = "<tr>";
+    //
+    //_.each(horizontalLineSummary, function (item) {
+    //
+    //    horizontalLineSummaryHtml += "<td>" + item + "</td>";
+    //
+    //    if ((j + 1) % 5 == 0) {
+    //        horizontalLineSummaryHtml += "<tr></tr>"
+    //    }
+    //    j++;
+    //});
 
-    _.each(horizontalLineSummary, function (item) {
 
-        horizontalLineSummaryHtml += "<td>" + item + "</td>";
+    backSlashSummaryHtml = NS.getGoalArraySummaryHtml(backSlashSummary);
+    slashSummaryHtml = NS.getGoalArraySummaryHtml(slashSummary);
 
-        if ((j + 1) % 5 == 0) {
-            horizontalLineSummaryHtml += "<tr></tr>"
-        }
-        j++;
-    });
-
-    var b = 0;
-    backSlashSummaryHtml = "<tr>";
-
-    _.each(backSlashSummary, function (item) {
-
-        backSlashSummaryHtml += "<td>" + item + "</td>";
-
-        if ((b + 1) % 5 == 0) {
-            console.log('111');
-            backSlashSummaryHtml += "<tr></tr>"
-        }
-        b++;
-    });
-
-    var c = 0;
-    slashSummaryHtml = "<tr>";
-
-    _.each(slashSummary, function (item) {
-
-        slashSummaryHtml += "<td>" + item + "</td>";
-
-        if ((c + 1) % 5 == 0) {
-            console.log('111');
-            slashSummaryHtml += "<tr></tr>"
-        }
-        c++;
-    });
+    //var b = 0;
+    //backSlashSummaryHtml = "<tr>";
+    //
+    //_.each(backSlashSummary, function (item) {
+    //
+    //    backSlashSummaryHtml += "<td>" + item + "</td>";
+    //
+    //    if ((b + 1) % 5 == 0) {
+    //        console.log('111');
+    //        backSlashSummaryHtml += "<tr></tr>"
+    //    }
+    //    b++;
+    //});
+    //
+    //var c = 0;
+    //slashSummaryHtml = "<tr>";
+    //
+    //_.each(slashSummary, function (item) {
+    //
+    //    slashSummaryHtml += "<td>" + item + "</td>";
+    //
+    //    if ((c + 1) % 5 == 0) {
+    //        console.log('111');
+    //        slashSummaryHtml += "<tr></tr>"
+    //    }
+    //    c++;
+    //});
 
     return {
         tableMatrix: matrixHtml1,
